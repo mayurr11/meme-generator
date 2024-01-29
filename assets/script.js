1af1f7469b928f6c2e40af6c27571d0808587143
mayur11 <mayurchauhan1107@gmail.com>
2024-01-29 23:47:25 +0530

    initial files

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+const memeGenerateBtn = document.querySelector('.meme-generator');
+const memeImage = document.querySelector('.meme-image');
+const memeTitle = document.querySelector('.meme-title');
+const memeAuthor = document.querySelector('.meme-author');
+
+
+const updateDetails = (url, title, author) => {
+    memeImage.setAttribute('src', url);
+    // memeTitle.innerText = title;
+    memeAuthor.innerText = author;
+}
+
+
+const generateMeme = () => {
+    fetch('https://meme-api.com/gimme/wholesomememes')
+    .then((res) => res.json()
+    .then((data) => {
+        updateDetails(data.url, data.title, data.author)
+    }))
+};
+
+
+memeGenerateBtn.addEventListener('click', generateMeme);
+
