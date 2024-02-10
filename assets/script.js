95bd2fa70ff309291b1a0381167440888d3b1f28
mayur11 <mayurchauhan1107@gmail.com>
2024-02-10 18:20:35 +0530

    modified

--- assets/script.js
+++ assets/script.js
@@ -6,13 +6,15 @@ const memeAuthor = document.querySelector('.meme-author');
 
 const updateDetails = (url, title, author) => {
     memeImage.setAttribute('src', url);
+    memeImage.style.marginBottom = '20px';
+    memeImage.style.marginTop = '20px';
     // memeTitle.innerText = title;
     memeAuthor.innerText = author;
 }
 
 
-const generateMeme = () => {
-    fetch('https://meme-api.com/gimme/wholesomememes')
+const generateMeme = async () => {
+    await fetch('https://meme-api.com/gimme/wholesomememes')
     .then((res) => res.json()
     .then((data) => {
         updateDetails(data.url, data.title, data.author)
